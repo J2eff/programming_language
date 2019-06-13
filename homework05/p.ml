b973df723f68483f06500f13b4bd96acfe8e98b0
orgensthe <w4102945@gmail.com>
2019-06-13 22:04:08 +0900

    99% complete Homework 5
    there is an error at let-polymorphic type inference
    ex iszero ture -> it can not make fail
    it should be fix

--- homework05/p.ml
+++ homework05/p.ml
@@ -9,17 +9,65 @@ module EEnv = struct
 end
 
 
+let rec change rootExp eenv = 
+  match rootExp with
+  | LET(var1,e1,e2) -> 
+      let e1' = change e1 eenv in
+      let eenv' = EEnv.extend (var1,e1') eenv in 
+      let e2' = change e2 eenv' in e2'
+
+  | TRUE -> TRUE
+	| FALSE -> FALSE
+  | CONST n ->  CONST n
+  | VAR x -> (try EEnv.find eenv x with _ -> VAR x)
+  | ADD(e1,e2) ->
+    let e1' =  change e1 eenv in 
+    let e2' =  change e2 eenv in  ADD(e1',e2')
+
+  | SUB(e1,e2) ->
+    let e1' =  change e1 eenv in 
+    let e2' =  change e2 eenv in  SUB(e1',e2')
+  | MUL(e1,e2)->
+    let e1' =  change e1 eenv in 
+    let e2' =  change e2 eenv in  MUL(e1',e2')
+
+  | DIV (e1,e2) ->
+    let e1' =  change e1 eenv in 
+    let e2' =  change e2 eenv in  DIV(e1',e2')
+
+  | ISZERO e1 ->
+    let e1' =  change e1 eenv in  (ISZERO e1')
+
+  | READ -> READ
+
+  | IF(e1,e2,e3) ->
+      let e1' =  change e1 eenv in 
+      let e2' =  change e2 eenv in
+      let e3' =  change e3 eenv in IF(e1',e2',e3')
+
+  | LETREC( varFun,varX, e1, e2)->
+    let e1' =  change e1 eenv  in LETREC(varFun,varX, e1', e2)
+    
+
+  | PROC(var1,e1)-> 
+    let e1' =  change e1 eenv in  PROC(var1,e1')
+
+  | CALL(e1,e2) ->
+    let e1' =  change e1 eenv in 
+    let e2' =  change e2 eenv in CALL(e1',e2')
+
 
 
 let expand: exp -> exp 
-= fun exp -> 
-  match exp with
-  | LET(var1,e1,e2) -> 
-  EEnv.extend (var1,e1) (EEnv.find [] var1)
-  | _ -> exp
+= fun exp -> change exp EEnv.empty
+
+
 
 (* typeof: Do not modify this function *)
 let typeof : exp -> typ 
 = fun exp -> 
 	let exp' = expand exp in 
 	M.typeof exp'  
+
+
+
