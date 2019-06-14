f9a74468d29e450351af06946433eb8ee8494ed5
orgensthe <w4102945@gmail.com>
2019-06-14 22:43:56 +0900

    finish homework5

--- homework05/p.ml
+++ homework05/p.ml
@@ -9,13 +9,35 @@ module EEnv = struct
 end
 
 
+let rec varCheck varX exp =
+  match exp with
+  | TRUE -> false
+	| FALSE -> false
+  | CONST n -> false
+  | VAR x -> if ((compare x varX)==0) then true else false
+  | ADD(e1,e2) -> (varCheck varX e1)||(varCheck varX e2)
+  | SUB(e1,e2) -> (varCheck varX e1)||(varCheck varX e2)
+  | MUL(e1,e2) -> (varCheck varX e1)||(varCheck varX e2)
+  | DIV(e1,e2) -> (varCheck varX e1)||(varCheck varX e2)
+  | ISZERO e1 -> (varCheck varX e1)
+  | READ -> false
+  | IF(e1,e2,e3) -> (varCheck varX e1)||(varCheck varX e2)||(varCheck varX e3)
+  | PROC(var1,e1)->  if((compare var1 varX)==0) then false else  (varCheck varX e1)
+  | CALL(e1,e2) ->(varCheck varX e1)
+  | LET(var1,e1,e2) ->  if((compare var1 varX)==0) then false else (varCheck var1 e2)
+  | LETREC(varOfF,varOfX,e1,e2) ->  if((compare varOfX varX)==0 || (compare varOfF varX)==0 ) then 
+  (varCheck varOfF e1)|| (varCheck varOfF e2)||(varCheck varOfX e1)|| (varCheck varOfX e2)
+  else (varCheck varX e1)|| (varCheck varX e2)
+
 let rec change rootExp eenv = 
   match rootExp with
   | LET(var1,e1,e2) -> 
+      let varcheck = varCheck var1 e2 in
+      if (varcheck) then 
       let e1' = change e1 eenv in
       let eenv' = EEnv.extend (var1,e1') eenv in 
       let e2' = change e2 eenv' in e2'
-
+      else rootExp
   | TRUE -> TRUE
 	| FALSE -> FALSE
   | CONST n ->  CONST n
